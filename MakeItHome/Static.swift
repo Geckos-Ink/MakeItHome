3822ec16f0acdfaaaa5b35d464e1bda5caf37c29
Riccardo Cecchini <rcecchini.ds@gmail.com>
2025-04-22 08:21:15 +0200

    more equilibrated update time

--- MakeItHome/Static.swift
+++ MakeItHome/Static.swift
@@ -112,7 +112,7 @@ public class Static {
     
     public static let ScreenRecorderHighPriorityFPS = 30 // FPS
     public static let CheckIfUpdateWindowScreenshotEvery : Double = 1 // seconds
-    public static let UpdateWindowScreenshotAfter : Double = 2 // seconds
+    public static let UpdateWindowScreenshotAfter : Double = 3 // seconds
     public static let EnableRecordingHalfInLowPriority = false
         
     public static let ClickMaximumDifference : Double = 0.5
@@ -129,7 +129,7 @@ public class Static {
     public static let WaitAfterSpaceChange = 3 // ticks
     
     // Clipboard
-    public static let ClipboardForgetElementsOlderThan = 30
+    public static let ClipboardForgetElementsOlderThan = 30 // ergo: maximum clipboard items
     
     ///# TOP BAR
     public static var EnableDragDropDetection : Bool = false
