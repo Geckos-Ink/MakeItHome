57ec6e5f3ca746fe5c5dec837ab5b36d985cb523
Riccardo Cecchini <rcecchini.ds@gmail.com>
2026-01-07 23:52:01 +0100

    removed half in low priority

--- MakeItHome/Static.swift
+++ MakeItHome/Static.swift
@@ -113,7 +113,7 @@ public class Static {
     public static let ScreenRecorderHighPriorityFPS = 30 // FPS
     public static let CheckIfUpdateWindowScreenshotEvery : Double = 0.5 // seconds
     public static let UpdateWindowScreenshotAfter : Double = 2.0 // seconds
-    public static let EnableRecordingHalfInLowPriority = true
+    public static let EnableRecordingHalfInLowPriority = false
     public static let WaitScreenshotAfterAboveBy = 0.3 // milliseconds
         
     public static let ClickMaximumDifference : Double = 0.5
